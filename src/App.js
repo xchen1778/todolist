a9d05493472041312ef73cbd6ff8c323e9cf3153
Xin Chen <xchen1778@gmail.com>
2022-10-03 17:36:02 -0400

    deploy website on firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Todolist from "./components/Todolist";
 import { useSelector } from "react-redux";
 import ThemeSwitch from "./components/ThemeSwitch";
+import BackgroundText from "./components/BackgroundText";
 
 function App() {
   const theme = useSelector((store) => store.theme);
+
   return (
     <div className="app" id={theme}>
+      <BackgroundText />
       <Todolist />
       <ThemeSwitch />
     </div>
---
+++ src/components/BackgroundText.js
@@ -0,0 +1,26 @@
+function BackgroundText() {
+  return (
+    <>
+      <p className="marquee1">
+        <span>TODOLIST.TODOLIST.TODOLIST.TODOLIST.</span>
+      </p>
+      <p className="marquee1 marquee1-sub">
+        <span>TODOLIST.TODOLIST.</span>
+      </p>
+      <p className="marquee2">
+        <span>TODOLIST.TODOLIST.</span>
+      </p>
+      <p className="marquee2 marquee2-sub">
+        <span>TODOLIST.TODOLIST.</span>
+      </p>
+      <p className="marquee3">
+        <span>TODOLIST.TODOLIST.TODOLIST.TODOLIST.</span>
+      </p>
+      <p className="marquee3 marquee3-sub">
+        <span>TODOLIST.TODOLIST.</span>
+      </p>
+    </>
+  );
+}
+
+export default BackgroundText;
